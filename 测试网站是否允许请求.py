2b0cade90c4a18478079e5263114d8e399662590
tyouki <tyouki@163.com>
2019-02-01 19:00:54 +0800

    xiu zheng

--- 测试网站是否允许请求.py
+++ 测试网站是否允许请求.py
@@ -4,9 +4,8 @@ from urllib.request import urlopen
 wangzhi=''
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 req = urllib.request.Request(url=wangzhi, headers=headers)
-request=urllib.request.Request(req)
 try:
-    urllib.request.urlopen(request)
+    urllib.request.urlopen(req)
     print('可以访问')
 except urllib.error.HTTPError as e:
     print(e.code)
