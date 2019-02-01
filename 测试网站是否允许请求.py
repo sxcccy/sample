58cd277e51579466ac958a78de1913436fa6ebbc
tyouki <tyouki@163.com>
2019-02-01 18:57:23 +0800

    add header

--- 测试网站是否允许请求.py
+++ 测试网站是否允许请求.py
@@ -1,7 +1,10 @@
 import urllib.request
 import urllib.error
 from urllib.request import urlopen
-request=urllib.request.Request('http://www.thevapingforum.com/Forum-General-Discussion')
+wangzhi=''
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+req = urllib.request.Request(url=wangzhi, headers=headers)
+request=urllib.request.Request(req)
 try:
     urllib.request.urlopen(request)
     print('可以访问')
